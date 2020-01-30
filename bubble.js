d73ae07f216287fb745258800d831515b8f34876
pdhoward <patrick.howard@hotmail.com>
2020-01-30 13:18:58 -0500

    template update

--- bubble.js
+++ bubble.js
@@ -3,27 +3,6 @@
 exports.Bubble = () => {
   return { 
   _id: null,
-  userId: "",
-  network: {
-    _id: null,
-    isPrivate: false,
-    isAvailable: false
-  },
-  meter: 0,
-  state: {
-    turn: 1,
-    context: 'welcome'
-  },   
-  intent: null,
-  intentArray: [],
-  callback: null,
-  message: null,
-  history: [],
-  reply: [],
-  seq: null,
-  slot: [],
-  timeStamp: null,
-  space:[],
-  system: []
- }
+  component: "bubble"
+  
 }
--- card.js
+++ card.js
@@ -3,27 +3,7 @@
 exports.Card = () => {
   return { 
   _id: null,
-  userId: "",
-  network: {
-    _id: null,
-    isPrivate: false,
-    isAvailable: false
-  },
-  meter: 0,
-  state: {
-    turn: 1,
-    context: 'welcome'
-  },   
-  intent: null,
-  intentArray: [],
-  callback: null,
-  message: null,
-  history: [],
-  reply: [],
-  seq: null,
-  slot: [],
-  timeStamp: null,
-  space:[],
-  system: []
- }
+  component: "card"
+  }
+ 
 }
